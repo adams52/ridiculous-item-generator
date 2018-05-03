459d255801936853f663598fc05159de4a26fa6a
adams52 <adams.b52@gmail.com>
2018-05-02 20:11:55 -0500

    Moved project into a Spring Boot web project so I could launch and test it easier

---
+++ RidiculousItemsWeb/src/main/resources/static/scripts/main.js
@@ -0,0 +1,7 @@
+
+$(function() {
+    // Populate using a Google spreadsheet ID (you must publish it first!)
+    // @see https://support.google.com/drive/answer/37579?hl=en
+    WTF.init( '1CU7QVSjTNmBy_5TwzOkVm-ujiD0b5z3Z3PJHioU6BvU' );
+
+});
